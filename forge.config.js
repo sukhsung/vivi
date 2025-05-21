ed0d7a46f02274f26c5640692264aa13cd1498e2
Suk Hyun Sung <sukhsung@umich.edu>
2025-05-21 16:26:53 -0400

    action modified

--- forge.config.js
+++ forge.config.js
@@ -3,7 +3,7 @@ const { FuseV1Options, FuseVersion } = require("@electron/fuses");
 
 module.exports = {
   packagerConfig: {
-    name: "Vivi",
+    name: "vivi",
     asar: true,
     icon: "./src/assets/vivi-icon.png",
   },
