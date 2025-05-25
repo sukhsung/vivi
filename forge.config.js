31f33f087541b6ac1fb9df0ded04330013c342ee
Suk Hyun Sung <sukhsung@umich.edu>
2025-05-25 01:41:14 -0400

    bug fig in electron-forge

--- forge.config.js
+++ forge.config.js
@@ -3,7 +3,7 @@ const { FuseV1Options, FuseVersion } = require("@electron/fuses");
 
 module.exports = {
   packagerConfig: {
-    name: "Vivi",
+    name: "vivi",
     executableName: "vivi", // File name of the actual binary
     asar: true,
     icon: "./src/assets/vivi-icon.png",
