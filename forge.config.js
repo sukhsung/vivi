49046e219d4dac07763fda8dd76cd1c39b6ba31d
Suk Hyun Sung <sukhsung@umich.edu>
2025-06-05 00:22:44 -0400

    github action modified

--- forge.config.js
+++ forge.config.js
@@ -58,10 +58,10 @@ module.exports = {
     },
   ],
   plugins: [
-    {
-      name: "@electron-forge/plugin-auto-unpack-natives",
-      config: {},
-    },
+    // {
+    //   name: "@electron-forge/plugin-auto-unpack-natives",
+    //   config: {},
+    // },
     // Fuses are used to enable/disable various Electron functionality
     // at package time, before code signing the application
     new FusesPlugin({
